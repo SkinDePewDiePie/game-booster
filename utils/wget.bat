f8edb828e930210ee32a27e5e4b527a71770501d
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-06-26 16:31:30 +0200

    Update wget.bat

--- utils/wget.bat
+++ utils/wget.bat
@@ -1,8 +1,17 @@
 @ECHO OFF
-if "%~0" equ ""( goto :help )
-if "%~1" equ ""( goto :help )
+if "%~0" equ ""(
+  goto :help
+
+) else(
+
+  if "%~1" equ ""(
+    goto :help
+ 
+  )
+)
 
 :help
 echo WGET for Windows
 echo Command format:
-echo wget.bat [37m[(file to download)[0m
+echo wget.bat [37m(file to download)[0m [37m(save to the file)[0m
+echo Created by MisterMatteo_o.

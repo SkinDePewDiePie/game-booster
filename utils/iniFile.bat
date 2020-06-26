3a8093b18798eec3f759f08323ec8c19c285a83e
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-06-26 19:07:06 +0200

    Update iniFile.bat

--- utils/iniFile.bat
+++ utils/iniFile.bat
@@ -14,3 +14,18 @@ set "param=^%~2" !
 set "param=!param:"=\"!"
 findstr /p /A:%1 "." "!param!\..\X" nul
 <nul set /p ".=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
+
+:help
+echo INI File for Windows
+echo.
+echo Command format:
+echo %~n0
+call :color 0F "[key] (subKey) +"
+echo.
+call :color 07 "  - key: "
+call :color 0F "For getting the values of a specified key."
+call :color 07 "  - subKey: " 
+call :color 0F "(Optional) For getting a value of a specified sub-key."
+echo.
+echo Created by MisterMatteo_o.
+echo.

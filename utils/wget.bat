ddb00a6954b7ae18b7f650287791c16585715da1
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-06-26 18:43:03 +0200

    Update wget.bat

--- utils/wget.bat
+++ utils/wget.bat
@@ -1,28 +1,22 @@
 @ECHO OFF
 if "%~1" equ ""(
   goto :help
-
 )
 
 if "%~2" equ ""(
-    goto :help
+  goto :help
 )
 
 :color
 set "param=^%~2" !
-
 set "param=!param:"=\"!"
 findstr /p /A:%1 "." "!param!\..\X" nul
 <nul set /p ".=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
 
-
 set /a url="%~1"
 set /a file="%~2"
 
-if
-
-"%~3" neq ""(
-
+if "%~3" neq ""(
   set /a timeout=%~3
 ) else(
   set /a timeout=5
@@ -35,14 +29,13 @@ bitsadmin /resume wgettingAFile > nul
 bitsadmin /setproxysettings wgettingAFile AUTODETECT > nul
 
 set /a attempts=0
-
 :repeat
 set /a attempts +=1
 if "%~attempts%" equ "10"(
-  echo A timeout has occcured. Check your internet connection and retry.
+  call :color 0C "A timeout has occcured. Check your internet connection and retry."
   exit
 )
-bitsadmin /info wgettingAFile /verbose | find "STATE: ERROR" > nul 2>&1 && bitsadmin /cancel wgettingAFile && echo An error has occured. Retry.
+bitsadmin /info wgettingAFile /verbose | find "STATE: ERROR" > nul 2>&1 && bitsadmin /cancel wgettingAFile && call :color 0C "An error has occured. Retry."
 bitsadmin /info wgettingAFile /verbose | find "STATE: TRANSFERRED" > nul 2>&1 && goto :finish
 
 w32tm /stripchart /computer:localhost /period:1 /dataonly /samples:%timeout% > nul 2>&1
@@ -50,24 +43,21 @@ goto :repeat
 
 :finish
 bitsadmin /complete wgettingAFile > nul
-echo Whgetting [37m"%url%"[0 is finished.
+call :color 07 "Wgetting"
+call :color 0F "%url% is finished."
 exit
-
-
-
 
 :help
 echo WGET for Windows
 echo.
 echo Command format:
-echo %~n0 [url] [file]
-
-
-
-
+echo %~n0
+call :color 0F "[url] [file]"
 echo.
-echo - url: For wgetting a file on t he World-Wide Web.
-echo - file: For saving the wgetted file with a name.
+call :color 07 "  - url: "
+call :color 0F "For wgetting a file on the World-Wide Web."
+call :color 07 "  - file: " 
+call :color 0F "For saving the wgetted file with a name."
 echo.
 echo Created by MisterMatteo_o.
 echo.

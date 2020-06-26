36efdf390a97b1684c98c7f27066fe47c1290f52
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-06-26 23:00:11 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,28 +6,31 @@ if %errorLevel% equ 1(
   del "%temp%\uacPrompt.vbs"
 )
 
-"%CD%\utils\getIniFile.bat"
+"%CD%\utils\getIniValue.bat" Config "%CD%\config.ini" Folder > %temp%\folderToInstall.tmp
+set /a path=<"%temp%\folderToInstall.tmp"
+del "%temp%\folderToInstall.tmp"
 
 for %%i in(0 1 2) do(
   set /a points=%%i
 
   if %points% equ 0(
-     cls
-     echo Ajout des valeurs sur le registre.
-     reg add HKCR\exefile\shell\game_booster
+    cls
+    echo Ajout des valeurs sur le registre.
+    reg add HKCR\exefile\shell\game_booster
+    reg add HKCR\exefile\shell\game_booster /v @ /t REG_SZ /d "Open with Game Booster"
+    reg add HKCR\exefile\shell\game_booster /v Icon /t REG_SZ /d "%path%\icon.ico"
   )
 
   if %points% equ 1(
-     cls
-     echo Ajout des valeurs sur le registre..
-     reg add HKCR\exefile\shell\game_booster /v @ /t REG_SZ /d "Open with Game Booster"
-     reg add HKCR\exefile\shell\game_booster /v Icon /t REG_SZ /d ""
+    cls
+    echo Ajout des valeurs sur le registre..
+     
   )
 
   if %points% equ 2(
-     cls
-     echo Ajout des valeurs sur le registre...
-     set /a points=3
+    cls
+    echo Ajout des valeurs sur le registre...
+    set /a points=3
   )
 )
 

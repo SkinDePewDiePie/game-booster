d09c5b19f192bfdf2dd47b1e35c46f4c287fc974
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-06-27 14:46:36 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,6 +10,12 @@ if %errorLevel% equ 1(
 set /a path=<"%temp%\folderToInstall.tmp"
 del "%temp%\folderToInstall.tmp"
 
+reg query HCKR\exefile\shell\game_booster > nul
+if %errorlevel% equ 0(
+  echo Game Booster is already installed.
+  goto :end
+)
+
 for %%i in(0 1 2) do(
   set /a points=%%i
 
@@ -44,9 +50,11 @@ for %%i in(0 1 2) do(
     reg add HKCR\exefile\shell\game_booster_admin\command /v @ /t REG_SZ /d "cmd /c \"start /min %path%\\app\\booster.vbs /withUAC \"%1\"\""
     reg add HKCR\exefile\shell\game_booster_admin\command /v IsolatedCommand /t REG_SZ /d "cmd /c \"start /min %path%\\app\\booster.vbs /withUAC \"%1\"\""
 
+    goto :end
     set /a points=3
   )
 )
 
+:end
 timeout /t 03 /nobreak > nul
 exit
